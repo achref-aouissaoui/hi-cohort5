219fb205888846b559d2d1de4505fea9b2a568b9
achref-aouissaoui <medachrefaouissaoui@gmail.com>
2020-06-22 00:58:38 +0100

    assessment after modification

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-function sum(numberOne, numberTwo){
-	return numberTwo + numberOne ;
-}
+ function guessMyNumber(n) { 
+ var upperBound = 5;
+ if (n > upperBound) { 
+ return 'Out of bounds! Please try a number between 0 and upperBound.'; 
+ } else if (n === randInt(upperBound)) { 
+ return 'You guessed my number! the right number is'randInt(upperBound); 
+ } 
+ return 'Nope! That wasnt it!'; 
+ } 
+ function randInt(n) { 
+ return Math.floor(Math.random() * (n + 1)) 
+ }
